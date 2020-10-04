71b1349713a2721825d6bad890d376fd73de6ed1
Rene Lochner <school@renelochner.at>
2020-10-04 18:54:30 +0200

    Implement CD manager - part 1

---
+++ WPF_CdManager1Template/CdManager/AddCdWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using CdManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CdManager
+{
+    /// <summary>
+    /// Interaction logic for AddCdWindow.xaml
+    /// </summary>
+    public partial class AddCdWindow : Window
+    {
+        Cd newCd;
+        public AddCdWindow()
+        {
+            InitializeComponent();
+            Loaded += new RoutedEventHandler(AddCdWindow_Loaded);
+        }
+
+        void AddCdWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnSave.Click += new RoutedEventHandler(btnSave_Click);
+            btnCancel.Click += new RoutedEventHandler(btnCancel_Click);
+
+            newCd = new Cd();
+            newCd.AlbumTitle = "< hier Titel eingeben >";
+            grdCdFields.DataContext = newCd;
+        }
+
+        void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Repository.GetInstance().AddCd(newCd);
+            Close();
+        }
+
+        void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ WPF_CdManager1Template/CdManager/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CdManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CdManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Cd> cds;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Repository rep = Repository.GetInstance();
+            cds = rep.GetAllCds();
+            lbxCds.ItemsSource = cds;
+
+            btnNew.Click += new RoutedEventHandler(btnNew_Click);
+        }
+
+        void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            AddCdWindow addCdWindow = new AddCdWindow();
+            addCdWindow.ShowDialog();
+            cds = Repository.GetInstance().GetAllCds();
+            lbxCds.ItemsSource = cds;
+        }
+    }
+}
